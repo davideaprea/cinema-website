23b652682b8d609488711b48d62ca56cb769402f
daviaprea <apredav@gmail.com>
2024-02-13 09:26:50 +0100

    refactored styles partials

--- capProjFrontEnd/src/app/features/booking/guards/saved-booking.guard.ts
+++
@@ -1,5 +0,0 @@
-import { CanDeactivateFn } from '@angular/router';
-
-export const savedBookingGuard: CanDeactivateFn<unknown> = (component, currentRoute, currentState, nextState) => {
-  return true;
-};
